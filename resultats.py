39bc995d49078c543dc560b066ba805844f82078
Whitypanzer <duvalvlad@gmail.com>
2025-03-08 11:13:58 -0300

    bg

--- resultats.py
+++ resultats.py
@@ -1,8 +1,8 @@
 resultats = {
-"black-templar":"u es un croisé fanatique, animé par une foi inébranlable. Pour toi, seule la guerre compte, et l’Empereur est un Dieu absolu que tu honores par le feu et l’acier.",
+"black-templar" : "Tu es un croisé fanatique, animé par une foi inébranlable. Pour toi, seule la guerre compte, et l’Empereur est un Dieu absolu que tu honores par le feu et l’acier.",
 
 "grey-wolf":"Tu es un guerrier bestial, féroce et indépendant. La guerre est ton terrain de chasse, et tu suis ton propre code d’honneur plutôt que des dogmes rigides.",
 
-"grey-knight":"Tu es l’élite ultime contre le Chaos, un guerrier purifié par l’Empereur lui-même. Armé de pouvoirs psychiques et d’un entraînement sans égal, tu es l’incarnation de la justice impériale.",
+"grey-knight" : "Tu es l’élite ultime contre le Chaos, un guerrier purifié par l’Empereur lui-même. Armé de pouvoirs psychiques et d’un entraînement sans égal, tu es l’incarnation de la justice impériale.",
 
 }
